2dc160e43d1d1489c965724e32f2b878a6986d81
nathanremond <99425832+nathanremond@users.noreply.github.com>
2025-04-15 10:05:45 +0200

    fix: routes /login + /register (#4)

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,16 @@
 import "../styles/global.css";
+import { AuthProvider } from "../context/AuthContext";
 
 import Header from "../layout/header";
 import Footer from "../layout/footer";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthProvider>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </AuthProvider>
   );
 }
 
