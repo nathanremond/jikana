5c884749f4bb12f5051ab1c67e9851606a43dc60
Jilen5 <158263580+Jilen5@users.noreply.github.com>
2025-04-15 17:29:47 +0200

    feat: ajout de page d'accueil (#8)
    Co-authored-by: Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const [movie, setMovie] = useState(null);
+  const router = useRouter();
+  
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/movie`)
+      .then((res) => res.json())
+      .then((data) => setMovie(data))
+      .catch((err) => console.error("Erreur :", err));
+  }, []);
+
   return (
     <div className="accueil_container">
-      <h1>Bonjour</h1>
+      <section>
+        <h2>Les film</h2>
+        <div className="accueil-film-list">
+          {!movie ? (
+            <p>Chargement...</p>
+          ) : movie.length === 0 ? (
+            <p>Aucun film.</p>
+          ) : (
+            movie.map((movie) => (
+              <div className="accueil-movies-card" key={movie.id_movie}>
+                <a href={`/movie/${movie.id_movie}`}>
+                  <img src={`/films/img${movie.name}.png`} alt={movie.name} />
+                  <h3>{movie.name}</h3>
+                </a>
+              </div>
+            ))
+          )}
+        </div>
+      </section>
     </div>
   );
 }
