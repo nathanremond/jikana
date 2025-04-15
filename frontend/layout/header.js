5da5ac8361007cb06bde871f964a525e035f5d9b
Jilen5 <158263580+Jilen5@users.noreply.github.com>
2025-04-15 17:07:13 +0200

    feat: ajout du header (#6)
    Co-authored-by: Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>

--- frontend/layout/header.js
+++ frontend/layout/header.js
@@ -2,7 +2,30 @@ export default function Header() {
 
   return (
     <header>
-
+      <div>
+        {/* Logo */}
+        <img
+          src="imageJiKaNa.png"
+          alt="Logo"
+          onClick={() => router.push("/")}
+        />
+      </div>
+      <div>
+        {/* Barre de recherche */}
+        <input
+          type="text"
+          placeholder="Rechercher..."
+        />
+      </div>
+      <div>
+        {/* Icône de connexion */}
+        <a href="/login" >
+          <img
+            src="icone_de_connexion.png"
+            alt="Icône de connexion"
+          />
+        </a>
+      </div>
     </header>
   );
 }
