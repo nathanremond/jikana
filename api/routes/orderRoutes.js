055051b77e26ad64948ac1f52292a487691069fb
nathanremond <perso.nathan.remond@gmail.com>
2025-04-16 22:58:47 +0200

    fix: import route order

--- api/routes/orderRoutes.js
+++ api/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const OrderModel = require("../models/ordermodel");
+const OrderModel = require("../models/orderModel");
 
 /**
  * @swagger
